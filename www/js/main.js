d50152a5a521d01c262cdfe32c8633dab73de1ed
yambal <jy@yambal.net>
2017-06-08 23:13:08 +0900

    key

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,9 @@
 (function($) {
     $(document).ready(function() {
+        console.log("ready");
+
         $('.keyboad-col').click(function(event) {
+            console.log("keyboad-col");
             var key = $(this).data("key");
             console.log(key);
         });
