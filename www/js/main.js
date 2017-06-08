bac40cf2e515b3ae2bfe4743b82d91182f3e518f
yambal <jy@yambal.net>
2017-06-08 23:10:06 +0900

    key

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,7 @@
 
 (function($) {
     $(document).ready(function() {
-        console.log("document ready");
+        var key = $('.keyboad-col').data("key");
+        console.log(key);
     });
 })(window.jQuery);
