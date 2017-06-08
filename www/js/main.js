69b252f22c39beca86723e01820d5bc19ba2e79c
yambal <jy@yambal.net>
2017-06-08 23:16:06 +0900

    key

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,34 @@
+//　====================================================
+// APP Start
+var isMonaca = false;
+var isDeviceready = false;
+var isFiredStart = false;
+if(typeof monaca !== "undefined"){
+    isMonaca = true;
+    document.addEventListener("deviceready", function() {
+        isDeviceready = true;
+        onDeviceReady();
+    }, false);
+};
+
 (function($) {
     $(document).ready(function() {
+        onDeviceReady();
+    });
+})(window.jQuery);
+
+function onDeviceReady(){
+        if(isMonaca && isDeviceready && !isFiredStart){
+            isFiredStart = true;
+            appStart();
+        }else if(!isMonaca && !isFiredStart){
+            isFiredStart = true;
+            appStart();
+        }
+        return;
+}
+
+function appStart(){
         console.log("ready");
 
         $('.keyboad-col').click(function(event) {
@@ -7,6 +36,4 @@
             var key = $(this).data("key");
             console.log(key);
         });
-
-    });
-})(window.jQuery);
+}
