8bd7a2f025557ea25474585a68bb90bbf3e075ba
yambal <jy@yambal.net>
2017-06-08 22:09:02 +0900

    4rd

--- www/js/main.js
+++ www/js/main.js
@@ -1,33 +1,6 @@
-//　====================================================
-// APP Start
-var isMonaca = false;
-var isDeviceready = false;
-var isFiredStart = false;
-//console.log(monaca);
-if (monaca) {
-    isMonaca = true;
-    document.addEventListener("deviceready", function() {
-        console.log("deviceready");
-        isDeviceready = true;
-        $(document).ready(function() {
-            console.log("document ready");
-            onDeviceReady();
-        });
-    }, false);
-} else {
-    isMonaca = false;
-    isDeviceready = false;
+
+(function($) {
     $(document).ready(function() {
         console.log("document ready");
-        onDeviceReady();
     });
-}
-
-function onDeviceReady() {
-    console.log("onDeviceReady");
-}
-
-//　====================================================
-function appStart() {
-    console.log("appStart");
-}
+})(window.jQuery);
