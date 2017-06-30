6ed2ff0de67c340bdbaac8cf54f663898b052f92
yambal <jy@yambal.net>
2017-06-30 19:58:44 +0900

    vibe

--- www/js/main.js
+++ www/js/main.js
@@ -110,7 +110,7 @@ var CoordinateCalculator = function() {
                     // 同じモードの場合は SubMode を変更する
                     toSubMode = getLatLonPairSubMode(mode, subMode);
                 }
-                navigator.vibrate(0.25);
+                navigator.vibrate(250);
                 _changeMode(val, toSubMode);
                 break;
 
